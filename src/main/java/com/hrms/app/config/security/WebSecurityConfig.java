64973eee4bd502a2c060b0e9b45a6603f16e2a35
Md Rezaul Karim <rezaul.km105@gmail.com>
2015-06-23 16:06:48 +0200

    init

--- src/main/java/com/hrms/app/config/security/WebSecurityConfig.java
+++ src/main/java/com/hrms/app/config/security/WebSecurityConfig.java
@@ -3,11 +3,9 @@ package com.hrms.app.config.security;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
-import org.springframework.security.config.annotation.authentication.configurers.ldap.LdapAuthenticationProviderConfigurer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
---
+++ src/main/resources/static/scriptlib/hrms/select2/select2.box.js
@@ -0,0 +1,9 @@
+
+//Select box for roles
+$('#roles').select2({
+	  placeholder: "Select a role(s)",
+	  allowClear: true
+});
+   
+      
+ 
